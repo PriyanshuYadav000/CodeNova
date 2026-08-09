9c6dd0d6f116a83cfbf91059be9ca269a5c62091
Priyanshu Yadav <priyanshu22209@gmail.com>
2026-08-09 15:53:27 +0530

    refactor(routes): clean and standardize API routes

--- backend/src/routes/submit.js
+++ backend/src/routes/submit.js
@@ -1,10 +1,10 @@
-
 const express = require('express');
-const submitRouter = express.Router();
-const userMiddleware = require("../middleware/userMiddleware");
-const {submitCode,runCode} = require("../controllers/userSubmission");
+const { submitCode, runCode } = require('../controllers/userSubmission');
+const userMiddleware = require('../middleware/userMiddleware');
 
-submitRouter.post("/submit/:id", userMiddleware, submitCode);
-submitRouter.post("/run/:id",userMiddleware,runCode);
+const submitRouter = express.Router();
+
+submitRouter.post('/submit/:id', userMiddleware, submitCode);
+submitRouter.post('/run/:id', userMiddleware, runCode);
 
 module.exports = submitRouter;
