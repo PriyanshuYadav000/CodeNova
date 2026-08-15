ab001a95f83e5283aa59c06a326751e91a8708d6
Priyanshu Yadav <priyanshu22209@gmail.com>
2026-08-15 23:45:50 +0530

    add Redis-backed rate limiting

--- backend/src/routes/submit.js
+++ backend/src/routes/submit.js
@@ -1,3 +1,5 @@
+
+const {codeRunRateLimiter,codeSubmitRateLimiter,} = require("../middleware/rateLimiter");
 const express = require('express');
 const { submitCode, runCode } = require('../controllers/userSubmission');
 const userMiddleware = require('../middleware/userMiddleware');
@@ -6,7 +8,7 @@ const { validateBody, validateSubmission } = require('../validators/requestValid
 
 const submitRouter = express.Router();
 
-submitRouter.post('/submit/:id', userMiddleware, validateBody(validateSubmission), asyncHandler(submitCode));
-submitRouter.post('/run/:id', userMiddleware, validateBody(validateSubmission), asyncHandler(runCode));
+submitRouter.post('/submit/:id', userMiddleware,codeSubmitRateLimiter, validateBody(validateSubmission), asyncHandler(submitCode));
+submitRouter.post('/run/:id', userMiddleware, codeRunRateLimiter,validateBody(validateSubmission), asyncHandler(runCode));
 
 module.exports = submitRouter;
